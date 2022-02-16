a17f10edfb933cd7afa180ab9509c5409ed10efa
LuLukas97 <lukas.lundblad@live.se>
2022-02-16 14:49:38 +0100

    Updated login page

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -6,36 +6,41 @@ import { Button } from "react-bootstrap";
 function loginPage() {
   return (
     <div className="loginpage">
+      <div className="signInContainer">
+        <div className="signInHeader">
+          <h1 className="h1LoginPage">SIGN IN</h1>
+        </div>
+      </div>
       <div className="containerLogin">
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>
-              Email<br></br>
-            </Form.Label>
-            <Form.Control
-              className="loginBoxes"
-              type="email"
-              placeholder="Enter email"
-            />
-            <Form.Text className="text-muted">
-              <br></br>
-            </Form.Text>
-          </Form.Group>
+        <div className="loginPageContainer">
+          <div className="loginPageForm">
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>
+                  <p className="loginPageFontStyle">Email</p>
+                </Form.Label>
+                <Form.Control className="loginBoxes" type="email" />
+              </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>
-              Password<br></br>
-            </Form.Label>
-            <Form.Control
-              className="loginBoxes"
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            LOGIN
-          </Button>
-        </Form>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>
+                  <span className="loginPageFontStyle">Password</span>
+                  <span className="loginPageForgotPassword">
+                    <a href="#">Forgot password?</a>
+                  </span>
+                </Form.Label>
+                <Form.Control className="loginBoxes" type="password" />
+              </Form.Group>
+              <Button
+                className="loginPageButton"
+                variant="primary"
+                type="submit"
+              >
+                <p className="LoginButtonText">LOGIN</p>
+              </Button>
+            </Form>
+          </div>
+        </div>
       </div>
     </div>
   );
