a88f934086c439d6aeca064f5a14c5484586648c
Benjis99 <91055304+Benjis99@users.noreply.github.com>
2022-02-16 14:18:49 +0100

    pictures for blogpost and small changes for text

--- src/blogPosts/pictureTextBox.js
+++ src/blogPosts/pictureTextBox.js
@@ -1,15 +1,25 @@
 import React from "react";
 import "./pictureTextBox.css";
 
-function pictureTextBox({ src, date, title, description, secondDescription }) {
+function pictureTextBox({
+  src,
+  date,
+  title,
+  line,
+  description,
+  secondDescription,
+  auth,
+}) {
   return (
     <div className="pictureTextBox">
       <img src={src} alt="" />
-      <div className="fgh">
+      <div className="descBox">
         <h2 className="dateText">{date}</h2>
         <p className="titleTextPosts">{title}</p>
+        <p className="lineText">{line}</p>
         <p className="descText">{description}</p>
         <p className="secDescText">{secondDescription}</p>
+        <p className="authText">{auth}</p>
       </div>
     </div>
   );
