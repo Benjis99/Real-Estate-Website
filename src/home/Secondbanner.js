78a17f510c366c57110bd69fbc0302a01fd33041
xLara <xlaraa@hotmail.se>
2022-02-16 16:04:40 +0100

    Fixade textbox på second banner

--- src/home/Secondbanner.js
+++ src/home/Secondbanner.js
@@ -3,6 +3,10 @@ import  "./Secondbanner.css"
 
 export default function Secondbanner() {
   return (
-    <div class="secondbanner"></div>
+    <div class="secondbanner">
+
+      <p class="secondtextbox">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At in tellus integer feugiat scelerisque varius. Scelerisque felis imperdiet proin fermentum leo vel. Morbi non arcu risus quis varius. Et tortor consequat id porta nibh venenatis cras. Tortor pretium viverra suspendisse potenti. Placerat orci nulla pellentesque dignissim enim sit amet venenatis urna. Eu ultrices vitae auctor eu. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Nullam ac tortor vitae purus.</p>
+    </div>
+
   )
 }
