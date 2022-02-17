ebd8e66f76d97e622ba6f10baa9c21627dbb8dd4
Benjis99 <91055304+Benjis99@users.noreply.github.com>
2022-02-17 13:04:14 +0100

    added inputs to contact page

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -7,8 +7,52 @@ import Form from "react-bootstrap/Form";
 function Contact() {
   return (
     <div className="contact">
-      <div className="contactBox">
-        <h1 className="contactTitle">Contact us!</h1>
+      <h1 className="contactTitle">Contact us</h1>
+      <div className="container-contact">
+        <div className="mb-3">
+          <input
+            type="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="First Name"
+          ></input>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Last Name"
+          ></input>
+        </div>
+        <div className="mb-3">
+          <input
+            type="email"
+            className="start-number-control"
+            id="exampleFormControlInput1"
+            placeholder="+46"
+          ></input>
+          <input
+            type="email"
+            className="mobile-control"
+            id="exampleFormControlInput1"
+            placeholder=""
+          ></input>
+        </div>
+        <div className="mb-3">
+          <input
+            type="email"
+            className="email-control"
+            id="exampleFormControlInput1"
+            placeholder="Email@address"
+          ></input>
+        </div>
+        <div className="mb-3">
+          <textarea
+            className="documents"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            placeholder="Write here"
+          ></textarea>
+        </div>
       </div>
     </div>
   );
