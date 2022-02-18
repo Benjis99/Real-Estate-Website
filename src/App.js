47b6c49e6701fc864585db7a97c7e2bac249b1da
Benjis99 <91055304+Benjis99@users.noreply.github.com>
2022-02-18 14:54:37 +0100

    Added header for about site and folder for loginpg

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import Header from "./Header";
 import Nav from "./Nav";
 import Footer from "./Footer";
 import Blog from "./blogPosts/Blog";
-import LoginPage from "./LoginPage";
+import LoginPage from "./login/LoginPage";
 import Contact from "./contact/Contact";
 import Listings from "./listings/Listings";
+import About from "./about/About";
 
 function App() {
   return (
     <div>
       <Header />
       <Nav />
-      <Blog />
+      <LoginPage />
       <Footer />
     </div>
   );
