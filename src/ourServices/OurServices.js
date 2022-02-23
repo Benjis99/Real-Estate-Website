7396796f14286d3f45f4a855861977bb1f13568a
LuLukas97 <lukas.lundblad@live.se>
2022-02-23 11:53:42 +0100

    Our services is now responsive to mobile 428x926

--- src/ourServices/OurServices.js
+++ src/ourServices/OurServices.js
@@ -16,15 +16,17 @@ function OurServices() {
         <div className="flex-item"></div>
         <div className="flex-item">
           <h1 className="servicesHeaderH1">Lorem ipsum</h1>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. At in
-          tellus integer feugiat scelerisque varius. Scelerisque felis imperdiet
-          proin fermentum leo vel. Morbi non arcu risus quis varius. Et tortor
-          consequat id porta nibh venenatis cras. Tortor pretium viverra
-          suspendisse potenti. Placerat orci nulla pellentesque dignissim enim
-          sit amet venenatis urna. Eu ultrices vitae auctor eu. Commodo nulla
-          facilisi nullam vehicula ipsum a arcu cursus. Nullam ac tortor vitae
-          purus
+          <p className="ourServicesBreadText">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. At in
+            tellus integer feugiat scelerisque varius. Scelerisque felis
+            imperdiet proin fermentum leo vel. Morbi non arcu risus quis varius.
+            Et tortor consequat id porta nibh venenatis cras. Tortor pretium
+            viverra suspendisse potenti. Placerat orci nulla pellentesque
+            dignissim enim sit amet venenatis urna. Eu ultrices vitae auctor eu.
+            Commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Nullam
+            ac tortor vitae purus
+          </p>
         </div>
         <div className="flex-item"></div>
       </div>
@@ -43,9 +45,9 @@ function OurServices() {
           <div className="flex-item-info"></div>
           <div className="flex-item-info">
             <div className="houseIcon"></div>
-            <p className="servicesIconTitle">LIST</p>{" "}
-            <p className="servicesIconTitleOrange">YOUR PROPTERY</p>
-            <p>
+            <div className="servicesIconTitle">LIST</div>{" "}
+            <div className="servicesIconTitleOrange">YOUR PROPTERY</div>
+            <p className="ourServicesBreadText">
               <br></br>
               If you are looking to sell your property, look no further. We will
               assist you in the process of selling your property. Fill in this
@@ -59,9 +61,9 @@ function OurServices() {
           </div>
           <div className="flex-item-info">
             <div className="propteryFinder"></div>
-            <p className="servicesIconTitle">PROPTERY</p>
-            <p className="servicesIconTitleOrange"> FINDER</p>
-            <p>
+            <div className="servicesIconTitle">PROPERTY</div>
+            <div className="servicesIconTitleOrangeProperty"> FINDER</div>
+            <p className="ourServicesBreadText">
               <br></br>
               If you are looking for the perfect home that will suit your family
               and your budget, then we are here to help. Fill in this form and
@@ -75,9 +77,9 @@ function OurServices() {
           </div>
           <div className="flex-item-info">
             <div className="alertBell"></div>
-            <p className="servicesIconTitle">PROPERTY</p>{" "}
-            <p className="servicesIconTitleOrange">ALERTS</p>
-            <p>
+            <div className="servicesIconTitle">PROPERTY</div>{" "}
+            <div className="servicesIconTitleOrangeProperty">ALERTS</div>
+            <p className="ourServicesBreadText">
               {" "}
               <br></br>
               Want to be kept in the loop when new properties are listed in your
