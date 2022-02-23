defcc12a487e557f26680f0d8ec57a276da6f4bc
Benjis99 <91055304+Benjis99@users.noreply.github.com>
2022-02-23 16:12:17 +0100

    create-blog done

--- src/createPost/CreatePost.js
+++ src/createPost/CreatePost.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./CreatePost.css";
+import { Button } from "react-bootstrap";
 
 function CreatePost() {
   return (
     <div className="createPost">
+      <h1 className="blog-create-title">Create Blog Post</h1>
       <section className="left-sides">
         <input
           type="blog-creater"
@@ -20,7 +22,13 @@ function CreatePost() {
       </section>
       <section className="middle-sections">
         <input
-          type="email"
+          type="title"
+          className="blog-title"
+          id="exampleFormControlInput1"
+          placeholder="Title"
+        ></input>
+        <input
+          type="date"
           className="blog-date"
           id="exampleFormControlInput1"
           placeholder="YYYY-MM-DD"
@@ -35,6 +43,7 @@ function CreatePost() {
       <section className="insert-picture">
         <h1 className="insert-picture-txt">Insert picture</h1>
         <input className="picture-insert" type="file" />
+        <Button className="post-blog">Post </Button>
       </section>
     </div>
   );
