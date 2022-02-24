b720c58e6b5374b8adb96f5b135f06d4c74d9bd3
Benjis99 <91055304+Benjis99@users.noreply.github.com>
2022-02-24 15:35:58 +0100

    added title to blogpost

--- src/blogPosts/BlogBox.js
+++ src/blogPosts/BlogBox.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./BlogBox.css";
 
-function BlogBox({ src, title, description, author }) {
+function BlogBox({ src, date, title, description, author }) {
   return (
     <div className="blogBox">
       <img src={src} alt="" />
       <div className="description">
-        <h4 className="title">{title}</h4>
+        <h4 className="date-title">{date}</h4>
+        <h4 className="title-blg">{title}</h4>
         <p className="descript">{description}</p>
         <p className="author">{author}</p>
       </div>
