ea5a477a315ed3729319377d8f1ebd2a79594baa
Benjis99 <91055304+Benjis99@users.noreply.github.com>
2022-03-02 18:28:42 +0100

    opacity removed

--- src/home/Secondbanner.js
+++ src/home/Secondbanner.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import  "./Secondbanner.css"
+import React from "react";
+import "./Secondbanner.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Secondbanner() {
+  const history = useNavigate();
   return (
     <div className="secondBanner">
-
       <h1 className="secondTitle">Lorem Ipsum</h1>
 
-      <p className="secondTextBox">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At in tellus integer feugiat scelerisque varius. Scelerisque felis imperdiet proin fermentum leo vel. Morbi non arcu risus quis varius. Et tortor consequat id porta nibh venenatis cras. Tortor pretium viverra suspendisse potenti. Placerat orci nulla pellentesque dignissim enim sit amet venenatis urna. Eu ultrices vitae auctor eu. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Nullam ac tortor vitae purus. 
-      <p className="quoteAuthor">-Lorem ipsum</p>
-      
-      <button className="listingObject">
-        Make listing
-      </button></p>
-     
+      <p className="secondTextBox">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. At in tellus integer
+        feugiat scelerisque varius. Scelerisque felis imperdiet proin fermentum
+        leo vel. Morbi non arcu risus quis varius. Et tortor consequat id porta
+        nibh venenatis cras. Tortor pretium viverra suspendisse potenti.
+        Placerat orci nulla pellentesque dignissim enim sit amet venenatis urna.
+        Eu ultrices vitae auctor eu. Commodo nulla facilisi nullam vehicula
+        ipsum a arcu cursus. Nullam ac tortor vitae purus.
+        <p className="quoteAuthor">-Lorem ipsum</p>
+        <button onClick={() => history("/contact")} className="listingObject">
+          Make listing
+        </button>
+      </p>
     </div>
-
-  )
+  );
 }
