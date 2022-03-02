b74ffab36a6b8eaac34facaa8d6fc9395edebf33
xLara <xlaraa@hotmail.se>
2022-03-02 18:52:35 +0100

    fixade click

--- src/home/Home.js
+++ src/home/Home.js
@@ -8,11 +8,13 @@ import house2 from "../pictures/house2.jpg";
 import house3 from "../pictures/house3.jpg";
 import house4 from "../pictures/house4.jpg";
 import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const [count, setCount] = useState(1842);
   const [middleCount, setMiddleCount] = useState(1413);
   const [right, setRightCount] = useState(1187);
+  const history = useNavigate();
 
   return (
     <div className="home">
@@ -106,7 +108,7 @@ function Home() {
           className="btn-middle"
           onClick={() => setMiddleCount(middleCount + 1)}
         >
-          <Button className="btn-two">View details</Button>
+          <Button onClick={() => history("/listings")} className="btn-two">View Listing</Button>
         </div>
         <div className="btn-right" onClick={() => setRightCount(right + 1)}>
           <Button className="btn-three">View details</Button>
